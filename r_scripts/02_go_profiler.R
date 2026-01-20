d69dabe4e53ebfeca508df9efd3fbd8cefc3455f
Claire Moulton-Brown <125563731+claire-moultonbrown@users.noreply.github.com>
2026-01-20 09:35:49 +0000

    Upload 02_go_profiler.R script

---
+++ r_scripts/02_go_profiler.R
@@ -0,0 +1,24 @@
+#!/usr/bin/env Rscript
+
+suppressPackageStartupMessages({
+  library(tidyverse)
+  library(gprofiler2)
+})
+
+proj <- "/path/to/project"
+deg <- read_csv(file.path(proj, "results_edgeR_deg_amf.csv"))
+
+sig_genes <- deg %>%
+  filter(is_deg) %>%
+  pull(gene_id)
+
+# Adjust organism if you are using Ensembl Plants or custom identifiers.
+# If gene IDs are MorexV3 gene IDs, you may need to map them first.
+gp <- gost(
+  query = sig_genes,
+  organism = "hordeum_vulgare",
+  correction_method = "fdr"
+)
+
+saveRDS(gp, file.path(proj, "gprofiler_amf_gost.rds"))
+write_csv(gp$result, file.path(proj, "gprofiler_amf_results.csv"))
